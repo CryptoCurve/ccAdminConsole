8041bd13f41b47129adb8639fbcd6a8ff7e5ec17
anton <anton@mfin.co.za>
2018-06-06 06:43:22 +0200

    Commit
    Signed-off-by: anton <anton@mfin.co.za>

--- src/config/production.config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-  apiUrl: "https://admin.cryptocurve.xyz/",
-  apiHeader: "Basic QTEyNTk3OEY3NkYxODA3NkVBMkQzNjY2QzAzOTE1QzM1OTVFODc5RUE2MEQ3NTQzNjg2NEJFMUE2MDJCNjA4QzpGMDNFODc5M0FBMjk4OTRFMkVBRjc1QUFERjY0MDA5RUFFQTk5MjMyMzI5OUU4M0RCODYwOTU4MENGMDBFQzJB"
-};
-
-module.exports = config;
